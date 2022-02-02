a4e54a446e7c38b476b34e2b58cbadf729d7a09c
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-02 18:55:50 -0300

    conteudo if else.

---
+++ 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/if-else.js
@@ -0,0 +1,9 @@
+const notaDesafio = 57;
+
+if (notaDesafio >= 80) {
+    console.log("Parabéns, você foi aprovado(a)!")
+}else if (notaDesafio >= 60) {
+    console.log("Você está na nossa lista de espera.")
+}else {
+    console.log("Você foi reprovado(a).")
+}
