deb47bd3b0ab378a14e55100fe43e7bf127a7898
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-04 13:50:24 -0300

    exercicio 6 concluido.

--- 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/4.1.6exercicio.js
+++ 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/4.1.6exercicio.js
@@ -2,10 +2,14 @@
 // faz.
 //--Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras 
 //maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
-const pecaDeXadrez = "rainha";
+const peca = 'Rainha';
+switch (peca.toLowerCase()) {
+  case 'rainha':
+    console.log('Rainha, mova para qualquer lado.');
+    break;
 
-if (pecaDeXadrez == "Rainha" && pecaDeXadrez == pecaDeXadrez.toLowerCase()) {
-    console.log(pecaDeXadrez , 'mova para qualquer lado');
-}else {
-  console.log('Bispo, para diagonal.');
+  default:
+    console.log('Bispo, para a diagonal.');
+    break;
 }
+
