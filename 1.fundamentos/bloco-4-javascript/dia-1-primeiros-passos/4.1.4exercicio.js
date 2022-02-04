7c6f24852a70ac385d8d464daf78c5ab96c8bbfd
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-03 21:19:35 -0300

    exercicio 4 concluido.

---
+++ 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/4.1.4exercicio.js
@@ -0,0 +1,10 @@
+//Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse 
+//valor for positivo, "negative" se for negativo e "zero" caso contrário.
+const a = 5;
+if (a > 0) {
+    console.log(a, "é positivo.");
+}else if (a < 0) {
+    console.log(a, "é negativo.");
+}else {
+    console.log(a, "é zero.");
+}
