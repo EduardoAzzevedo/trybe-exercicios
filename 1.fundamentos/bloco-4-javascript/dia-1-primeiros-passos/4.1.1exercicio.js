fb51da588410b7e95429f54e946a4183edcd01f9
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-03 21:00:04 -0300

    conclusao de exercicio 1.

---
+++ 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/4.1.1exercicio.js
@@ -0,0 +1,23 @@
+const a = 5;
+const b = 7;
+
+//Adição
+if (a == 5 || b == 7) {
+    console.log(a + b);
+}
+//Subtração
+if (a == 5 || b == 7) {
+    console.log(a - b);
+}
+//Multiplicação
+if (a == 5 || b == 7) {
+    console.log(a * b);
+}
+//Divisão
+if (a <= 5 && b >= 7) {
+    console.log(a / b);
+}
+//Módulo
+if (a === 5 && b === 7) {
+    console.log(a % b);
+}
