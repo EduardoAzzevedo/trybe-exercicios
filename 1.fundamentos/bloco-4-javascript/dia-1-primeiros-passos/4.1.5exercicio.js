a9a5a606636df99aef3d9cdcad6b222767c9e997
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-03 21:29:59 -0300

    exercicio 5 concluido.

---
+++ 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/4.1.5exercicio.js
@@ -0,0 +1,12 @@
+//Faça um programa que defina três constantes com os valores dos três ângulos internos de um 
+//triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false ,
+//caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+const a = 60
+const b = 60
+const c = 60
+
+if (a == 60 && b == 60 && c == 60) {
+    console.log("True");
+}else {
+    console.log("False! Não é um Triangulo!");
+}
