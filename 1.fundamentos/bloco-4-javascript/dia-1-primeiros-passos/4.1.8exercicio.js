d4bf5f1f5d7bb25ac69261e890f488739df527c3
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-09 15:24:16 -0300

    conclusao do exercicio 8.

--- 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/4.1.8exercicio.js
+++ 1.fundamentos/bloco-4-javascript/dia-1-primeiros-passos/4.1.8exercicio.js
@@ -1,3 +1,6 @@
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
 const number1 = 5;
 const number2 = 10;
 const number3 = 15;
