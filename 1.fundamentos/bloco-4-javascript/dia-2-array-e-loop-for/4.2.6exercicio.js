53cf6b5bdd68ae72460624bf534e2d36e29c7066
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-11 10:13:53 -0300

    conclusao do exercicio 7.

--- 1.fundamentos/bloco-4-javascript/dia-2-array-e-loop-for/4.2.6exercicio.js
+++ 1.fundamentos/bloco-4-javascript/dia-2-array-e-loop-for/4.2.6exercicio.js
@@ -1,13 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let result = 0;
 
-for (let index = 0; index < numbers.length; index+= 1) {
-    if (number[index] % 2 !==  0 ) {
-         resultado += 1;
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    result += 1;
+  }
 }
-if(resultado === 0){
-    console.log('nenhum valor impar encontrado')
-}else {
-    console.log(resultado);
+
+if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
 }
