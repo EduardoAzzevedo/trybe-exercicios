e091034efdb625b4cb1a24a64ed6fddfc79d446c
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-11 15:06:11 -0300

    conclusao do exercicio 3.

--- 1.fundamentos/bloco-4-javascript/dia-3-logica-programacao-algoritimos/4.3.2exercicio.js
+++ 1.fundamentos/bloco-4-javascript/dia-3-logica-programacao-algoritimos/4.3.2exercicio.js
@@ -5,3 +5,20 @@ for (let index = word.length - 1; index >= 0; index-=1) {
 }
 console.log(inverso);
 // Um diferencial importante.
+//outra forma de se resolver
+let word = 'tryber';
+let reverseWord = '';
+
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+}
+
+console.log(reverseWord);
+
+//mais outra forma
+let word = 'tryber';
+let reverseWord = '';
+
+reverseWord = word.split('').reverse().join('');
+
+console.log(reverseWord);
