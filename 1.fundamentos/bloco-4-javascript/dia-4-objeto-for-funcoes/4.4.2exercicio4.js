9268a7550da9dc46cf2d3a9cb5b80eb5b32041e5
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-02-15 13:47:20 -0300

    conclusao do exercicio.

---
+++ 1.fundamentos/bloco-4-javascript/dia-4-objeto-for-funcoes/4.4.2exercicio4.js
@@ -0,0 +1,10 @@
+function biggestWord(array) {
+    let bigestWord = [];
+    for (let key in array) {
+        if (array[bigestWord] < array[index]) {
+            bigestWord.push([key]);
+        }
+    return bigestWord;
+    }
+}
+console.log(biggestWord(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
