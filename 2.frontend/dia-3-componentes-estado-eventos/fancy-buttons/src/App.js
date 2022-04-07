6f1dd499d56bb66e35d2c498865cb5a179ab50c4
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-04-07 19:01:42 -0300

    fixar eventos ate o 7.

--- 2.frontend/dia-3-componentes-estado-eventos/fancy-buttons/src/App.js
+++ 2.frontend/dia-3-componentes-estado-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,48 @@
 import './App.css';
 import React from 'react';
 
-function testeUm() {
-  console.log('Click Teste Um!');
-}
-
-function testeDois() {
-  console.log('Click Teste Dois!');
-}
-
-function testeTres() {
-  console.log('Click Teste Três!');
-}
-
 class App extends React.Component { // fazer uma div pra cada botão não funciona.
+  constructor() {
+    super()
+
+    this.state = {
+      cliquesTest1: 0,
+      cliquesTest2: 0,
+      cliquesTest3: 0,
+    }
+
+    this.testeUm = this.testeUm.bind(this)
+    this.testeDois = this.testeDois.bind(this)
+    this.testeTres = this.testeTres.bind(this)
+  }
+
+  testeUm() {
+    this.setState((estadoAnterior, _props) => ({
+      cliquesTest1: estadoAnterior.cliquesTest1 + 1
+    }))
+    console.log('Click Teste Um!');
+  }
+
+  testeDois() {
+    this.setState((estadoAnterior, _props) => ({
+      cliquesTest2: estadoAnterior.cliquesTest2 + 1
+    }))
+  console.log('Click Teste Dois!');
+  }
+
+  testeTres() {
+    this.setState((estadoAnterior, _props) => ({
+      cliquesTest3: estadoAnterior.cliquesTest3 + 1
+    }))
+    console.log('Click Teste Três!');
+  }
+
   render(){
   return (
     <div>
-      <button onClick={testeUm}> Teste 1</button>
-      <button onClick={testeDois}> Teste 2</button>
-      <button onClick={testeTres}> Teste 3</button>
+      <button onClick={this.testeUm}>{this.state.cliquesTest1}</button>
+      <button onClick={this.testeDois}>{this.state.cliquesTest2}</button>
+      <button onClick={this.testeTres}>{this.state.cliquesTest3}</button>
     </div>
     );
   }
